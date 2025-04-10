d8881c45d742f6b71950c5d2e32408a84b8d1f35
Matthew Barton <matthewbarton2910@gmail.com>
2025-04-10 08:44:39 -0700

    did everything u bum

--- ContentView.swift
+++ ContentView.swift
@@ -1,9 +1,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var  munber1 = 0
-    @State var number1 = 0
+    @State var  number1: Int?
+    @State var number2: Int?
+    @State var answer = 0
     var body: some View {
-        
+        HStack{
+            TextField("Enter Number 1", value: $number1, format: .number)
+            TextField("Enter Number 2", value: $number2, format: .number)
+        }
+        Button("Calculate"){
+            answer = (number1 ?? 0) + (number2 ?? 0)
+        }
+        Text("Answer: \(answer)")
     }
 }
